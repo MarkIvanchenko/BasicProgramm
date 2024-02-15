42c4191ad27ddc83aadba22a035143e0f5b682cd
Mark <mark@mail.ru>
2024-02-15 22:09:58 +0300

    Добавил деление

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -6,3 +6,19 @@ Console.WriteLine(sum);
 long L1 = 6;
 long sum2 = I1 + L1;
 Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + I1;
+
+int product1 = I1 * I2;
+long product2 =  L1 * I1;
+double product3 = I1 * d1;
+Console.WriteLine(product1);
+Console.WriteLine(product2);
+Console.WriteLine(product3);
+
+int quotione1 = I1 / I2;
+Console.WriteLine(quotione1);
+
+double quotione2 = I1 / d1;
+Console.WriteLine(quotione2);
