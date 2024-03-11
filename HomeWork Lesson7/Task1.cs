d94aba3604be9c9b94dec3c1a34a62aab0dfe227
MarkIvanchenko <158240906+MarkIvanchenko@users.noreply.github.com>
2024-03-11 23:48:02 +0300

    HomeWork Lesson 7

---
+++ HomeWork Lesson7/Task1.cs
@@ -0,0 +1,40 @@
+﻿﻿// Задача 1: 
+// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+
+void Gap(int M, int N)
+{
+    if (N == M)
+    {
+        Console.Write("Промежуток из натуральных чисел от M до N составляет: " + N + " ");
+        return;
+    }     
+    Gap(M, N-1);
+    Console.Write(N + " "); 
+ }
+
+Console.Clear();
+Console.WriteLine("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+if(M < 0)
+    {   
+        M = M * -1;
+    }
+    else if (N < 0)
+    {
+        N = N * -1;
+    }
+    else if(N < M)
+    {
+        int num = 0;
+        num = N;
+        N = M;
+        M = num;
+    }
+Gap(M , N);
+
+
---
+++ HomeWork Lesson7/Task3.cs
@@ -0,0 +1,82 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы. Пример вы найдете в конце презентации к семинару.
+
+
+int[] Arr(int n)  // Создад массива размерностью n с заполнением случайными числами
+{
+    int[] array = new int[n]; 
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+    return array;
+}
+
+void Rec (int [] arr, int i = 0) // Рекурсия на вывод массива в развароте
+{
+    if (i == arr.Length)
+    { 
+    return;
+    }
+    Rec(arr, i + 1);
+    Console.Write(arr[i] + "\t");
+}
+
+void Print(int[] arr) // Вывод на печать заданного массива
+{
+    foreach (var item in arr)
+    {
+         Console.Write(item + "\t");
+    }    
+    Console.WriteLine();
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = Arr(n);
+Console.WriteLine();
+Console.Write("Заданный массив: ");
+Print(array);
+Console.WriteLine();
+Console.Write("Массив в развароте: ");
+Rec(array);
+
+
+
+
+// int[] FillArrayRandomly(int n)
+// {
+//     int[] array = new int[n];
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(1, 10);
+//     }
+//     return array;
+// }
+
+// void RecursiveArrayPrint (int [] arr, int index = 0)
+// {
+//     if (index == arr.Length)
+//     { 
+//     return;
+//     }
+//     RecursiveArrayPrint(arr, index + 1);
+//     Console.Write($"{arr[index]} ");
+
+// }
+
+// void PrintArray(int[] arr)
+// {
+//     foreach (var item in arr)
+//     {
+//         Console.Write($"{item} ");
+//     }    
+//     Console.WriteLine();
+// }
+
+// Console.WriteLine("Введите размерность массива: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int [] array = FillArrayRandomly(m);
+// PrintArray(array);
+// RecursiveArrayPrint(array);
